a607e92e61f2eb5d0cee9a4542076cb8f84022c3
Insignificant_Guy <zakramisaway@gmail.com>
2021-03-27 04:57:26 +0530

    Finished Building the Game

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Tic-Tac-Toe
 A simple Tic-Tac-Toe game build using HTML,CSS and Vanilla JS
+
+##Functionality
+The game is designed to be played by two players. The first player is automatcally assigned the 'X' sign, while the next player is assigned the 'O' character. 
+The players can then move as they wish and when any of the players win, the game displays the message.
--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+// Variable to toggle the Player
 var i=0;
+//Array to Store the block number.
 var arr=[-1,-1,-1,-1,-1,-1,-1,-1,-1];
 
+//Function that generates the X or O sign in the Game
 function generate(el){
 	var index = Array.from(document.getElementsByClassName('box')).findIndex(x => x === el);
 	if(arr[index]==-1){
@@ -14,10 +17,66 @@ function generate(el){
 		}
 		i++;
 	}
-	check(index);
+	var x=check(index);
+	if(x==1){
+		if(arr[index]==1){
+			alert("Player A has Won");
+		}
+		else{
+			alert("Player B has Won");
+		}
+		setInterval(()=>{},3000);
+		window.location.reload();
+	}
+	if(x!=1&&i==9){
+		alert("Match Ended in a Draw");
+		setInterval(()=>{},3000);
+		window.location.reload();
+	}
 };
 
+// Function that Checks if any person won the game or not.
 function check(x){
-	var victory=""+arr[x]+arr[x]+arr[x];
-	
+	var bet=0;
+	if(x==0||x==1||x==2){
+		if(arr[0]==arr[1]&&arr[1]==arr[2]){
+			bet=1;
+		}
+	}
+	if(x==0||x==3||x==6){
+		if(arr[0]==arr[3]&&arr[3]==arr[6]){
+			bet=1;
+		}
+	}
+	if(x==0||x==4||x==8){
+		if(arr[0]==arr[4]&&arr[4]==arr[8]){
+			bet=1;
+		}
+	}
+	if(x==1||x==4||x==7){
+		if(arr[1]==arr[4]&&arr[4]==arr[7]){
+			bet=1;
+		}
+	}
+	if(x==2||x==5||x==8){
+		if(arr[2]==arr[5]&&arr[5]==arr[8]){
+			bet=1;
+		}
+	}
+	if(x==2||x==4||x==6){
+		if(arr[2]==arr[4]&&arr[4]==arr[6]){
+			bet=1;
+		}
+	}
+	if(x==3||x==4||x==5){
+		if(arr[3]==arr[4]&&arr[4]==arr[5]){
+			bet=1;
+		}
+	}
+	if(x==6||x==7||x==8){
+		if(arr[6]==arr[7]&&arr[7]==arr[8]){
+			bet=1;
+		}
+	}
+	return bet;
 }
